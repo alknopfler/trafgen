4e40952d662ad33a8cb5446a1a40a2b6040ed03e
spyroot@gmail.com <spyroot@gmail.com>
2024-04-11 10:36:41 +0400

    Add monitor for softnet

--- src/run_monitor_pps.sh
+++ src/run_monitor_pps.sh
@@ -347,13 +347,18 @@ function run_trafgen_inter_pod() {
         local opt_cmd
         opt_cmd=""
 
-        echo "Starting on pod $_tx_pod_name with core $_default_core and pps $pps for ${DEFAULT_TIMEOUT} sec taskset $_task_set_core dev $DEFAULT_IF_NAME profile $trafgen_udp_file2"
+        echo "Starting on pod $_tx_pod_name with core $_default_core and pps "\
+        "$pps for ${DEFAULT_TIMEOUT} sec taskset $_task_set_core dev "\
+        "$DEFAULT_IF_NAME profile $trafgen_udp_file2"
 
         if [ "$USE_TASKSET" == "true" ]; then
             opt_cmd="taskset -c $_task_set_core"
         fi
 
-        kubectl exec "$_tx_pod_name" -- timeout "${DEFAULT_TIMEOUT}s" /usr/local/sbin/trafgen --cpp --dev "$DEFAULT_IF_NAME" -i "$trafgen_udp_file2" --no-sock-mem --rate "${pps}pps" --bind-cpus "$_default_core" -H > /dev/null 2>&1 &
+        kubectl exec "$_tx_pod_name" -- timeout "${DEFAULT_TIMEOUT}s" \
+        /usr/local/sbin/trafgen --cpp --dev "$DEFAULT_IF_NAME" -i \
+        "$trafgen_udp_file2" --no-sock-mem --rate "${pps}pps" --bind-cpus "$_default_core" -H > /dev/null 2>&1 || true &
+
         local trafgen_pid_var="trafgen_pid$i"
         declare "$trafgen_pid_var"=$!
     done
